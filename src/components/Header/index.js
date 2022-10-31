25fd501bf4e4d97719ac0077f7d1e4432e933ad5
JoãoFerreiraNewWay <joao.ferreira@gruponewway.com.br>
2022-10-31 19:51:12 -0300

    feat: add less content in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,8 @@ import { TouchableOpacity, View } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import IconMI from 'react-native-vector-icons/MaterialIcons'
 import { HeaderContainer, UserButton, WelcomeText } from './styles'
-export default function Header(props){
+export default function Header({ lessContent = false }){
+  
   return (
     <HeaderContainer>
       <View style={{marginTop: 24, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
@@ -13,7 +14,7 @@ export default function Header(props){
           <IconMI name="help-outline" size={20} color="#fff"/>
         </TouchableOpacity>
       </View>
-      <WelcomeText>Olá, João</WelcomeText>
+      {!lessContent && ( <WelcomeText>Olá, João</WelcomeText> )}
     </HeaderContainer>
   )
 }
