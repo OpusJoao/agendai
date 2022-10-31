03ce153b33592b5d20fb9305bea9795b374ae146
JoãoFerreiraNewWay <joao.ferreira@gruponewway.com.br>
2022-10-31 19:50:58 -0300

    feat: create component card

---
+++ src/components/Cards/Card/index.js
@@ -0,0 +1,36 @@
+import { View, TouchableOpacity, Text } from "react-native";
+import { CardBody, Duration, Title, Time, LineTop, Line, LinkContainer } from "./styles";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+export default function Card({duration = '', title = '', time = '', lessContent = false}) {
+  return (
+    <CardBody>
+      <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+      <LineTop />
+        <View style={{ padding: 8 }}>
+          <Duration>{duration}</Duration>
+          <Title>{title}</Title>
+          <Time>{time}</Time>
+        </View>
+        <TouchableOpacity style={{ display:'flex', alignItems: 'center', justifyContent: 'center', paddingRight: 16}}>
+          <Icon name="angle-right" size={24} color="#666" />
+        </TouchableOpacity>
+      </View>
+      <Line />
+
+      {!lessContent && (
+        <LinkContainer>
+          <TouchableOpacity>
+            <Text style={{fontSize: 12, color: "#FCA351"}}>Copiar Link</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={{fontSize: 12, color: "#FCA351"}}>Copiar link de uso único</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={{fontSize: 12, color: "#FCA351"}}>Duplicar</Text>
+          </TouchableOpacity>
+        </LinkContainer>
+      )}
+    </CardBody>
+  )
+}
