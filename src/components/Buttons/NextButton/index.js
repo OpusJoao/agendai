b3515dc2182a382891179d8ae3cfce58f4af9d2a
JoãoFerreiraNewWay <joao.ferreira@gruponewway.com.br>
2022-11-01 10:08:53 -0300

    refactor: change style from NextButton

--- src/components/Buttons/NextButton/index.js
+++ src/components/Buttons/NextButton/index.js
@@ -6,23 +6,18 @@ export default function NextButton({onPress}){
         <TouchableOpacity
             onPress={onPress}
             style={{
-                margin: 12,
-                width: 64,
+                width: '100%',
                 height: 64,
                 justifyContent: 'center',
                 alignItems: 'center',
                 padding: 10,
                 borderRadius: 14,
-                shadowColor: '#171717',
-                shadowOffset: {width: 0, height: 1},
-                shadowOpacity: 0.2,
                 shadowRadius: 7,
-                backgroundColor: 'white',
-                elevation: 20
+                backgroundColor: '#FCA351',
             }}
         >
             <Text style={{fontSize: 18}}>
-                <Icon name="arrowright" size={30} color="#000" />
+                <Icon name="arrowright" size={30} color="#fff" />
             </Text>
         </TouchableOpacity>
     )
