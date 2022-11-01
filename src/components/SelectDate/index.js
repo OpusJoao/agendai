f8a4bdf832c278e82b71bfeb6afdc3748c752e32
JoãoFerreiraNewWay <joao.ferreira@gruponewway.com.br>
2022-11-01 10:08:24 -0300

    refactor: card component

--- src/components/SelectDate/index.js
+++ src/components/SelectDate/index.js
@@ -2,9 +2,11 @@ import {Text, TouchableOpacity, View} from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { useState } from "react";
 import NextButton from "../Buttons/NextButton";
+import Card from "../Cards/Card";
 
 function SelectDate ( props ){
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [date, setDate] = useState('')
 
     const showDatePicker = () => {
         setDatePickerVisibility(true);
@@ -15,7 +17,7 @@ function SelectDate ( props ){
     };
 
     const handleConfirm = (date) => {
-        console.warn("A date has been picked: ", date);
+        // setDate(date)
         hideDatePicker();
     };
 
@@ -24,30 +26,50 @@ function SelectDate ( props ){
     }
 
     return (
-        <View style={{height:"100%", display: "flex", justifyContent: "space-around", alignItems: "center",  paddingHorizontal: 24}}>
-            <Text style={{margin: 8, fontSize: 24}}>Selecione o dia</Text>
-            <TouchableOpacity
-                onPress={showDatePicker}
-                style={{
-                    margin: 24,
-                    width: "100%",
-                    maxWidth: 120,
-                    height: 40,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    padding: 10,
-                    borderRadius: 14,
-                    backgroundColor: "#2496ef",
-                }}
-            >
-                <Text style={{color: "white", fontSize: 18}}>Selecionar</Text>
-            </TouchableOpacity>
-            <DateTimePickerModal
-                isVisible={isDatePickerVisible}
-                mode="date"
-                onConfirm={handleConfirm}
-                onCancel={hideDatePicker}
-            />
+        <View style={{height:"100%", display: "flex", justifyContent: "space-around",  paddingHorizontal: 16}}>
+            <Card>
+                <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <View style={{ padding: 16, width: '100%'}}>
+                    <Text style={{ width: '100%', textAlign: 'center' }}>Selecione o dia da reunião</Text>
+                    </View>
+                </View>
+            </Card>
+            <Card>
+                <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <View style={{ padding: 16, width: '100%', height: '' }}>
+                    <TouchableOpacity
+                        onPress={showDatePicker}
+                        style={{
+                            width: "100%",
+                            height: 40,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            padding: 10,
+                            borderRadius: 14,
+                            backgroundColor: "#2496ef",
+                        }}
+                    >
+                        <Text style={{color: "white", fontSize: 18}}>Selecionar</Text>
+                    </TouchableOpacity>
+                    <DateTimePickerModal
+                        isVisible={isDatePickerVisible}
+                        mode="date"
+                        onConfirm={handleConfirm}
+                        onCancel={hideDatePicker}
+                    />
+                    </View>
+                </View>
+            </Card>
+
+            <Card>
+                <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <View style={{ padding: 16, width: '100%'}}>
+                        <Text style={{ width: '100%', textAlign: 'center', color: '#7D7D7D' }}>Data selecionada</Text>
+                        <Text style={{ width: '100%', textAlign: 'center' }}>{date}</Text>
+                    </View>
+                </View>
+            </Card>
+            
             <NextButton onPress={handlePressNextButton}/>
 
         </View>
